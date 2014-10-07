54c3dd4153ca2f528c20c593023bcb8c79208d64
Joseph E. Roldan <omega6441@gmail.com>
2014-10-07 09:21:08 -0700

    my recent changes to my project

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -5,7 +5,12 @@ var screenwidth;
 var screenheight;
 
 function gameinitialize(){
-    
+    var canvas=element = document.getElementById("game-screen");
+    context=canvas.getcontext("2d");
+    screenwidth=window.innerWidth;
+    screenheight=window.innerHeight;
+    canvas.width =screenWidth;
+    canvas.height =screenHeight; 
 }
 function gameloop(){
     
