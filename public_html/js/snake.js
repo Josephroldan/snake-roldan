1f89fbc7cfc9278841783047c6cb2b2df5e3fead
Joseph E. Roldan <omega6441@gmail.com>
2014-10-08 09:20:37 -0700

    my new changes

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,11 +4,15 @@ var context;
 var screenwidth;
 var screenheight;
 
+gamedraw();
+gameinitialize();
 function gameinitialize(){
     var canvas=element = document.getElementById("game-screen");
     context=canvas.getcontext("2d");
+    
     screenwidth=window.innerWidth;
     screenheight=window.innerHeight;
+    
     canvas.width =screenWidth;
     canvas.height =screenHeight; 
 }
@@ -16,5 +20,7 @@ function gameloop(){
     
 }
 function gamedraw(){
+    context.fillstyle = "rgb(152, 156, 237)";
+    context.fillrect(0,0,screenwidth,screenheight);
     
 }
