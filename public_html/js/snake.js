b1b8146a29a108d9cfb60fcde9857f04e160bd74
Joseph E. Roldan <omega6441@gmail.com>
2014-10-14 09:13:35 -0700

    recent new changes to snake movement

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -9,43 +9,44 @@ var screenheight;
 gameInitialize();
 gameDraw();
 
-function gameInitialize(){
-    var canvas=element = document.getElementById("game-screen");
-    context=canvas.getContext("2d");
-    
-    screenwidth=window.innerWidth;
-    screenheight=window.innerHeight;
-    
-    canvas.width =screenwidth;
-    canvas.height =screenheight; 
+function gameInitialize() {
+    var canvas = element = document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+
+    screenwidth = window.innerWidth;
+    screenheight = window.innerHeight;
+
+    canvas.width = screenwidth;
+    canvas.height = screenheight;
 }
-function gameLoop(){
-    
+function gameLoop() {
+
 }
-function gameDraw(){
+function gameDraw() {
     context.fillStyle = "rgb(208,188,232)";
-    context.fillRect(0,0,screenwidth,screenheight);
+    context.fillRect(0, 0, screenwidth, screenheight);
 }
-function snakeInitialize (){
+function snakeInitialize() {
     snake = [];
     snakeSize = 20
-    snakeLength= 5;
-    
-    for(var index = 0; index < snakeLength; index++) {
-    
-   snake.push({
-       x:index,
-       y:0
-   })
-           
-                 }
+    snakeLength = 100;
+
+    for (var index = 1; index <= 0; index--) {
+        snake.push({
+            x: index,
+            y: 0
+        });
+    }
 }
-function snakeDraw (){
-  for(var index = 0; index < snake.Length; index++) {
-      context.fillStyle = "black";
-      context.fillRect(snake[index].x*snakeSize, snake[index]);
-  }  
+function snakeDraw() {
+    for (var index = 0; index < snake.Length; index++) {
+        context.fillStyle = "black";
+        context.fillRect(snake[index].x * snakeSize, snake[index]);
+    }
 }
-function snakeUpdate (){
-    
+function snakeUpdate() {
+var snakeHeadX = snake[0].x;
+var snakeHeadY = snake[0].y;
+
+var snakeTail = snake.pop();
 }
